1a928622e08089504ff2b1f72c60be856597be0a
JMACBigMan <jemclaurin@gmail.com>
2020-06-15 18:36:56 -0400

    Updates

---
+++ precache-manifest.70ec029a1f5af480b624752a80c1eca7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35449a802d675c9a762597803d27272d",
+    "url": "/greeting/index.html"
+  },
+  {
+    "revision": "bcf3a411b9a8c93d6376",
+    "url": "/greeting/static/css/main.f1766645.chunk.css"
+  },
+  {
+    "revision": "68e86fecdcb421903088",
+    "url": "/greeting/static/js/2.8594b76c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/greeting/static/js/2.8594b76c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcf3a411b9a8c93d6376",
+    "url": "/greeting/static/js/main.afa31ede.chunk.js"
+  },
+  {
+    "revision": "e366637a94a952f6c74b",
+    "url": "/greeting/static/js/runtime-main.8e98f56a.js"
+  }
+]);
